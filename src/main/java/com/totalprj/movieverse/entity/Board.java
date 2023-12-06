b252c6785b0697a88f13102072af5e5958dcfcef
Leejaewon2 <dlwodnjs2669@gmail.com>
2023-12-06 09:40:07 +0900

    재원 Board, Category, Comment entity 추가

---
+++ src/main/java/com/totalprj/movieverse/entity/Board.java
@@ -0,0 +1,39 @@
+package com.totalprj.movieverse.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+@ToString
+public class Board {
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    @Column(name = "board_title", nullable = false)
+    private String title;
+    @Column(name = "board_content", nullable = false)
+    private String boardContent;
+    private String image;
+    @Column(name = "gather_type")
+    private String gatherType;
+    private int count;
+    @Column(name = "registration_date")
+    private LocalDateTime regdate;
+    @PrePersist
+    public void prepersist() {regdate = LocalDateTime.now();}
+
+}
---
+++ src/main/java/com/totalprj/movieverse/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.totalprj.movieverse.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@ToString
+public class Comment {
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+    @Column(name = "comment_content", nullable = false)
+    private String commentContent;
+
+}
