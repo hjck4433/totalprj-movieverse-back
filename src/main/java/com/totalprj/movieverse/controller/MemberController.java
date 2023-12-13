c13506eafb7d9233005b0f37b48352cd0a1fc9ae
11yhj <uouo_@naver.com>
2023-12-13 16:02:29 +0900

    현주 회원정보 상세 조회 추가

--- src/main/java/com/totalprj/movieverse/controller/MemberController.java
+++ src/main/java/com/totalprj/movieverse/controller/MemberController.java
@@ -1,4 +1,30 @@
 package com.totalprj.movieverse.controller;
 
+
+import com.totalprj.movieverse.dto.MemberResDto;
+import com.totalprj.movieverse.security.SecurityUtil;
+import com.totalprj.movieverse.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    // 회원 상세 조회
+    @GetMapping("/detail")
+    public ResponseEntity<MemberResDto> memberDetail(){
+        Long id = SecurityUtil.getCurrentMemberId();
+        log.info("id : {} ", id);
+        MemberResDto memberResDto = memberService.getMemberDetail(id);
+        return ResponseEntity.ok(memberResDto);
+    }
+
+
 }
--- src/main/java/com/totalprj/movieverse/service/MemberService.java
+++ src/main/java/com/totalprj/movieverse/service/MemberService.java
@@ -1,7 +1,41 @@
 package com.totalprj.movieverse.service;
 
+import com.totalprj.movieverse.dto.MemberResDto;
+import com.totalprj.movieverse.entity.Member;
+import com.totalprj.movieverse.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // 회원 상세 조회
+    public MemberResDto getMemberDetail(Long id){
+        Member member = memberRepository.findById(id).orElseThrow(()-> new RuntimeException("해당 회원이 존재하지 않습니다."));
+        return convertEntityToDto(member);
+    }
+
+    // 회원 엔티티를 회원 DTO로 변환
+    private MemberResDto convertEntityToDto(Member member){
+        MemberResDto memberResDto = new MemberResDto();
+        memberResDto.setEmail(member.getEmail());
+        memberResDto.setName(member.getName());
+        memberResDto.setAlias(member.getAlias());
+        memberResDto.setPhone(member.getPhone());
+        memberResDto.setAddr(member.getAddr());
+        memberResDto.setImage(member.getImage());
+        memberResDto.setIsMembership(member.isMembership());
+        return memberResDto;
+    }
+
+
 }
+
+
