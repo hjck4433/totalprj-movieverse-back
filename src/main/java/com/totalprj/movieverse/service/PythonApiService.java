d53bac8897a8ca0ba98b0728e7397958f06bf545
Leejaewon2 <dlwodnjs2669@gmail.com>
2023-12-15 14:32:24 +0900

    재원 PythonApiService 정보 추출 성공

--- src/main/java/com/totalprj/movieverse/service/PythonApiService.java
+++ src/main/java/com/totalprj/movieverse/service/PythonApiService.java
@@ -1,61 +1,53 @@
 package com.totalprj.movieverse.service;
 
-import com.totalprj.movieverse.entity.Movie;
-import com.totalprj.movieverse.repository.MovieRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PythonApiService {
-    private final MovieRepository movieRepository;
 
-//     파이썬 DB 내용 불러오기
-    public static void main(String[] args) {
-        String apiUrl = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2";
+//    @Scheduled(cron = "0 1 * * * *")
+    @PostConstruct
+    public void startScheduler(){
+        log.info("schedule start!");
+        List<Map<String, List<Map<String, String>>>> response = fetchDataFromPythonServer();
+        log.info("python response : {}", response);
+    }
 
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+    public List<Map<String, List<Map<String, String>>>> fetchDataFromPythonServer() {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "http://127.0.0.1:5000/api/kmdblist";
+        ResponseEntity<List<Map<String, List<Map<String, String>>>>> responseEntity = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Map<String, List<Map<String, String>>>>>() {});
 
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
-
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
-
-                String jsonResponse = response.toString();
-                System.out.println(jsonResponse);
-
-            } else {
-                System.out.println("HTTP GET 요청 실패 ");
-            }
-            connection.disconnect();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            log.error("Request failed with status code: {}", responseEntity.getStatusCodeValue());
+            return null;
         }
     }
+
+
+
+
 
 
 // DB에 저장한 정보 비우기
