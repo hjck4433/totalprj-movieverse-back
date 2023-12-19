7d32ca075da7ca830da4444b720ca33c7a6b4ab4
11yhj <uouo_@naver.com>
2023-12-19 18:02:31 +0900

    현주 faqDto, faqService, faqController 추가

---
+++ src/main/java/com/totalprj/movieverse/controller/FaqController.java
@@ -0,0 +1,32 @@
+package com.totalprj.movieverse.controller;
+
+import com.totalprj.movieverse.dto.FaqDto;
+import com.totalprj.movieverse.service.FaqService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+
+@RestController
+@Slf4j
+@RequestMapping("/faq")
+@RequiredArgsConstructor
+public class FaqController {
+    private final FaqService faqService;
+
+    @PostMapping("/new")
+    public ResponseEntity<FaqDto> createFaq(@RequestParam String faqName){
+        FaqDto faqDto = faqService.createFaq(faqName); // 새 faq생성
+        log.info("FAQ 저장하기: {}", faqDto);
+        return ResponseEntity.ok(faqDto); // 저장된 faq dto 반환
+
+    }
+}
---
+++ src/main/java/com/totalprj/movieverse/dto/FaqDto.java
@@ -0,0 +1,10 @@
+package com.totalprj.movieverse.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class FaqDto {
+    private Long faqId;
+    private String faqName;
+}
