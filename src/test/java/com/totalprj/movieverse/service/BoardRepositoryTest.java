4ea4b462ba41fb4cff31c2c768cd55e0f87f2d84
TommyLee <kokokorkr@naver.com>
2023-12-22 21:07:48 +0900

    세웅 테스트

--- src/test/java/com/totalprj/movieverse/service/BoardRepositoryTest.java
+++ src/test/java/com/totalprj/movieverse/service/BoardRepositoryTest.java
@@ -19,6 +19,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -56,6 +59,7 @@ public class BoardRepositoryTest {
         return category;
     }
 
+
     @Test
     @DisplayName("게시글 카테고리 및 회원 정보 매핑 테스트")
     public void findCategoryAndMemberSaveTest(){
@@ -80,6 +84,67 @@ public class BoardRepositoryTest {
         Board savedBoard = boardRepository.findById(board.getId()).orElseThrow(EntityNotFoundException::new);
 
         System.out.println("savedBoard test 결과 : " + savedBoard);
+    }
+
+
+    // 게시글 정보 생성
+    private Board createBoardInfo(Member member, Category category, String title, String content) {
+        Board board = new Board();
+        board.setMember(member);
+        board.setCategory(category);
+        board.setGatherType("오프라인");
+        board.setTitle(title);
+        board.setBoardContent(content);
+        board.setImage("햄스터이미지.jpg");
+        board.setCount(0);
+        return board;
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회")
+    public void findAllBoardsTest() {
+        Member member = createMemberInfo();
+        memberRepository.save(member);
+
+        Category category = createCategoryInfo();
+        categoryRepository.save(category);
+
+        Board board1 = createBoardInfo(member, category, "게시글 1", "게시글 내용 1");
+        boardRepository.save(board1);
+
+        Board board2 = createBoardInfo(member, category, "게시글 2", "게시글 내용 2");
+        boardRepository.save(board2);
+
+        List<Board> boards = boardRepository.findAll();
+
+        assertNotNull(boards);
+        assertEquals(2, boards.size());
+        assertEquals("게시글 1", boards.get(0).getTitle());
+        assertEquals("게시글 2", boards.get(1).getTitle());
+
+        System.out.println("전체 게시글 개수: " + boards.size());
+        System.out.println("전체 게시글 조회 결과: " + boards);
+    }
+
+    @Test
+    @DisplayName("게시글 상세 조회 테스트")
+    public void findBoardByIdTest() {
+        Member member = createMemberInfo();
+        memberRepository.save(member);
+
+        Category category = createCategoryInfo();
+        categoryRepository.save(category);
+
+        Board board = createBoardInfo(member, category, "게시글 제목", "게시글 내용");
+        boardRepository.save(board);
+
+        Board foundBoard = boardRepository.findById(board.getId()).orElse(null);
+
+        assertNotNull(foundBoard);
+        assertEquals("게시글 제목", foundBoard.getTitle());
+        assertEquals("게시글 내용", foundBoard.getBoardContent());
+
+        System.out.println("게시글 상세 조회 결과: " + foundBoard);
     }
 
 }
